e9a8840cf8c0ad1a77f16df023ccab54321eed9e
Elvan H <elvanhd@gmail.com>
2021-10-11 21:34:13 +0700

    adding contact-create component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ContactCreateComponent } from './contacts/contact-create/contact-create.component';
+import { ContactsDashboardComponent } from './contacts/contacts-dashboard/contacts-dashboard.component';
 import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
   {
     path: '',
     component: HomeComponent,
+  },
+  {
+    path: 'contacts',
+    component: ContactsDashboardComponent,
+  },
+  {
+    path: 'contact-create',
+    component: ContactCreateComponent,
+  },
+  {
+    path: '**',
+    redirectTo: '',
   },
 ];
 
--- src/app/contacts/contacts.module.ts
+++ src/app/contacts/contacts.module.ts
@@ -1,9 +1,10 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { ContactsDashboardComponent } from './contacts-dashboard/contacts-dashboard.component';
+import { ContactCreateComponent } from './contact-create/contact-create.component';
 
 @NgModule({
-  declarations: [ContactsDashboardComponent],
+  declarations: [ContactsDashboardComponent, ContactCreateComponent],
   imports: [CommonModule],
 })
 export class ContactsModule {}
