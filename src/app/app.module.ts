7707a288164f1fea642883ee3c1903d59c477324
Elvan H <elvanhd@gmail.com>
2021-10-11 21:48:23 +0700

    creating an example data

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NavbarComponent } from './navbar/navbar.component';
+import { ContactsModule } from './contacts/contacts.module';
 import { HomeComponent } from './home/home.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [AppComponent, NavbarComponent, HomeComponent],
-  imports: [BrowserModule, AppRoutingModule, NgbModule],
+  imports: [BrowserModule, AppRoutingModule, NgbModule, ContactsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/contacts/contacts-dashboard/contacts-dashboard.component.ts
+++ src/app/contacts/contacts-dashboard/contacts-dashboard.component.ts
@@ -3,13 +3,31 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-contacts-dashboard',
   templateUrl: './contacts-dashboard.component.html',
-  styleUrls: ['./contacts-dashboard.component.css']
+  styleUrls: ['./contacts-dashboard.component.css'],
 })
 export class ContactsDashboardComponent implements OnInit {
+  contacts = [
+    {
+      id: 1,
+      name: 'John Doe',
+      email: 'john@doe.com',
+      phone: '555-555-5555',
+    },
+    {
+      id: 2,
+      name: 'Aubrey Mall',
+      email: 'aubrey@mall.com',
+      phone: '555-555-6666',
+    },
+    {
+      id: 3,
+      name: 'Sam Smith',
+      email: 'sam@smith.com',
+      phone: '555-555-7777',
+    },
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
