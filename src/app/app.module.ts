b2ee1300eca9ce32027b74d975270277caf200f3
Elvan H <elvanhd@gmail.com>
2022-02-01 03:32:03 +0700

    add new contact form and validation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
+import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { AngularFireModule } from '@angular/fire';
 import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { environment } from '../environments/environment';
@@ -12,21 +14,25 @@ import { SidebarComponent } from './components/sidebar/sidebar.component';
 import { LoginComponent } from './features/account/login/login.component';
 import { RegisterComponent } from './features/account/register/register.component';
 import { ContactDashboardComponent } from './features/contacts/contact-dashboard/contact-dashboard.component';
+import { ContactFormComponent } from './features/contacts/contact-form/contact-form.component';
 import { HomeComponent } from './features/home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ContactDashboardComponent,
-    HomeComponent,
-    LoginComponent,
     NavbarComponent,
     NotFoundComponent,
-    RegisterComponent,
     SidebarComponent,
+    LoginComponent,
+    RegisterComponent,
+    ContactDashboardComponent,
+    ContactFormComponent,
+    HomeComponent,
   ],
   imports: [
     BrowserModule,
+    CommonModule,
+    FormsModule,
     AppRoutingModule,
     AngularFireModule.initializeApp(environment.firebase),
     AngularFirestoreModule,
--- src/app/features/contacts/contact-form/contact-form.component.ts
+++ src/app/features/contacts/contact-form/contact-form.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import faker from '@faker-js/faker';
+import { Contact } from 'src/app/models/contact';
 
 @Component({
   selector: 'app-contact-form',
@@ -6,7 +8,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contact-form.component.css'],
 })
 export class ContactFormComponent implements OnInit {
-  constructor() {}
+  contact: Contact;
+
+  constructor() {
+    this.contact = {
+      firstName: faker.name.firstName(),
+      lastName: faker.name.lastName(),
+      email: faker.internet.email().toLowerCase(),
+      phone: faker.phone.phoneNumber(),
+    };
+  }
 
   ngOnInit(): void {}
+
+  save() {
+    console.log('save');
+  }
 }
