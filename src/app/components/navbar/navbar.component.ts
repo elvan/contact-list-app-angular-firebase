5301bdac64dbfdc02156bfe8107f41f96a6ff18e
Elvan H <elvanhd@gmail.com>
2022-02-04 16:16:26 +0700

    refactoring the navbar

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import firebase from 'firebase/app';
+import { Observable } from 'rxjs';
 import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
@@ -9,21 +10,17 @@ import { AuthService } from 'src/app/services/auth.service';
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css'],
 })
-export class NavbarComponent implements OnInit {
+export class NavbarComponent {
   collapsed = true;
 
-  currentUser: firebase.User | null = null;
+  user$: Observable<firebase.User | null>;
 
   constructor(
     private router: Router,
     private modalService: NgbModal,
     private authService: AuthService
-  ) {}
-
-  ngOnInit(): void {
-    this.authService.getUser().subscribe((currentUser) => {
-      this.currentUser = currentUser;
-    });
+  ) {
+    this.user$ = this.authService.getUser();
   }
 
   openModal(content: any): void {
